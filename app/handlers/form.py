bd6c13afb76278dab92c598a7ac8a2fce774bac9
Paulina <fxcklleewyb@gmail.com>
2025-06-08 23:25:51 +0300

    feat/to update form

--- app/handlers/form.py
+++ app/handlers/form.py
@@ -6,6 +6,8 @@ from app.keyboards.agreement import get_agreement_kb
 from app.states.form import FormStates
 from app.database.models import save_user_data
 
+from app.keyboards.reply.form_keyboard import region_button, experience_button, study_level_button, confim_button
+
 router = Router()
 
 
@@ -26,15 +28,77 @@ async def ask_agreement(msg: Message, state: FSMContext):
 
 @router.callback_query(FormStates.waiting_agreement, F.data == 'agree')
 async def agreed(call: CallbackQuery, state: FSMContext):
-    await call.message.answer('Введите ваше ФИО:')
+    await call.message.answer('Для начала укажите в сообщении ваши ФИО:')
     await state.set_state(FormStates.waiting_name)
     await call.answer()
 
 @router.message(FormStates.waiting_name)
 async def get_name(msg: Message, state: FSMContext):
     await state.update_data(name=msg.text)
-    await msg.answer('Введите номер телефона:')
+    await msg.answer('Ваш контактный номер телефона:')
     await state.set_state(FormStates.waiting_phone)
+
+@router.message(FormStates.waiting_phone)
+async def get_name(msg: Message, state: FSMContext):
+    await state.update_data(number=msg.text)
+    await msg.answer('Ваш электронный адрес:')
+    await state.set_state(FormStates.waiting_email)
+
+@router.message(FormStates.waiting_email)
+async def get_name(msg: Message, state: FSMContext):
+    await state.update_data(email=msg.text)
+    await msg.answer('Выбранный регион поиска:', reply_markup=region_button())
+    await state.set_state(FormStates.waiting_region)
+
+@router.message(FormStates.waiting_region)
+async def get_name(msg: Message, state: FSMContext):
+    await state.update_data(region=msg.text)
+    await msg.answer('Желаемая должность:')
+    await state.set_state(FormStates.waiting_position)
+
+@router.message(FormStates.waiting_position)
+async def get_name(msg: Message, state: FSMContext):
+    await state.update_data(position=msg.text)
+    await msg.answer('Ваш стаж:', reply_markup=experience_button())
+    await state.set_state(FormStates.waiting_experience)
+
+@router.message(FormStates.waiting_experience)
+async def get_name(msg: Message, state: FSMContext):
+    await state.update_data(experience=msg.text)
+    await msg.answer('Ваш уровень образования:', reply_markup=study_level_button())
+    await state.set_state(FormStates.waiting_study_level)
+
+@router.message(FormStates.waiting_study_level)
+async def get_name(msg: Message, state: FSMContext):
+    await state.update_data(study_level=msg.text)
+    data = await state.get_data()
+
+    await msg.answer(
+        text= f'''
+ФИО:   
+{data['name']}
+
+Номер телефона:
+{data['number']}
+
+Email:
+{data['email']}
+
+Регион поиска:
+{data['region']}
+
+Должность:
+{data['position']}
+
+Опыт работы:
+{data['experience']}
+
+Образование:
+{data['study_level']}
+    ''',
+    reply_markup=confim_button()
+    )
+    await state.set_state(FormStates.waiting_confirm)
 
 @router.message(FormStates.waiting_phone)
 async def get_phone(msg: Message, state: FSMContext):
