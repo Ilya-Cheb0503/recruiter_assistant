e42ec1852aac86f02250b787401aa3f93649fac1
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2025-06-08 21:39:28 +0300

    update form handler with callback_query

--- app/handlers/form.py
+++ app/handlers/form.py
@@ -1,3 +1,4 @@
+from operator import call
 from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery, FSInputFile
 from aiogram.fsm.context import FSMContext
@@ -6,6 +7,15 @@ from app.states.form import FormStates
 from app.database.models import save_user_data
 
 router = Router()
+
+
+@router.callback_query(F.data == 'submit_form')
+async def job_search_handler(callback: CallbackQuery, state: FSMContext):
+    pdf = FSInputFile('static/agreement.pdf')
+    await callback.answer()
+    await callback.message.answer_document(pdf, caption='Вы согласны с политикой обработки данных?', reply_markup=get_agreement_kb())
+    await state.set_state(FormStates.waiting_agreement)
+
 
 @router.message(F.text.lower() == 'анкета')
 async def ask_agreement(msg: Message, state: FSMContext):
