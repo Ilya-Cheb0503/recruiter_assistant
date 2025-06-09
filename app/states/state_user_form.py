48dee9a67e00fb9f7f7ca1cc7b2443e28c0b143a
Paulina <fxcklleewyb@gmail.com>
2025-06-09 12:28:57 +0300

    chore: to rename files

---
+++ app/states/state_user_form.py
@@ -0,0 +1,12 @@
+from aiogram.fsm.state import StatesGroup, State
+
+class FormStates(StatesGroup):
+    waiting_agreement = State()
+    waiting_name = State()
+    waiting_phone = State()
+    waiting_email = State()
+    waiting_region = State()
+    waiting_position = State()
+    waiting_experience = State()
+    waiting_education_level = State()
+    waiting_confirm = State()
