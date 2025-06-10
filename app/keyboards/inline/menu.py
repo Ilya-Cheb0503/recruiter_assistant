52f83ce2df3712187fdbe8f3b5558ba1dadce9e4
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2025-06-10 21:22:01 +0300

    feat: add back keyboards functions

--- app/keyboards/inline/menu.py
+++ app/keyboards/inline/menu.py
@@ -1,7 +1,7 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import os
-from dotenv import load_dotenv
 
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from dotenv import load_dotenv
 
 load_dotenv()
 ADMIN_IDS = list(map(int, os.getenv("ADMINS", "").split(",")))
@@ -18,5 +18,28 @@ def get_main_menu(user_id: int) -> InlineKeyboardMarkup:
 
     if user_id in ADMIN_IDS:
         buttons.append([InlineKeyboardButton(text="Панель администратора", callback_data="admin_panel")])
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+
+def get_job_seeking_menu() -> InlineKeyboardMarkup:
+    buttons =[
+        [InlineKeyboardButton(text="Все вакансии", callback_data="all_vacancies")],
+        [InlineKeyboardButton(text="Вакансии по направлениям деятельности", callback_data="vacancies_by_keywords")],
+        [InlineKeyboardButton(text="Без опыта работы", callback_data="vacancies_no_experience")],
+        [InlineKeyboardButton(text="Главное меню", callback_data="main_menu")]
+    ]
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+
+def get_job_by_categories_menu() -> InlineKeyboardMarkup:
+    buttons =[
+        [InlineKeyboardButton(text="Руководители", callback_data="vacancies_with_management")],
+        [InlineKeyboardButton(text="ИТР", callback_data="vacancies_with_engineering")],
+        [InlineKeyboardButton(text="Рабочие", callback_data="vacancies_with_workers")],
+        [InlineKeyboardButton(text="Другие категории", callback_data="vacancies_with_other")],
+        [InlineKeyboardButton(text="Назад", callback_data="job_search")]
+    ]
 
     return InlineKeyboardMarkup(inline_keyboard=buttons)
