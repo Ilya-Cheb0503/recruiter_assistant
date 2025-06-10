efd44cbb3fb6d6811c658163eef71d872d598c7d
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2025-06-10 21:22:34 +0300

    feat: add job_seeking router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from app.config import load_config
-from app.handlers import start, common, form
+from app.handlers import start, common, form, job_seeking
 
 async def main():
     config = load_config()
@@ -11,5 +11,6 @@ async def main():
     dp.include_router(start.router)
     dp.include_router(common.router)
     dp.include_router(form.router)
+    dp.include_router(job_seeking.router)
 
     await dp.start_polling(bot)
