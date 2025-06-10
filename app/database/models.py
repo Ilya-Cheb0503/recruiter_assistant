20cad2adcd400192c0f84c2ef4eb41aac0c4f0d0
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2025-06-10 21:19:29 +0300

    feat: add user model to db

--- app/database/models.py
+++ app/database/models.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, Integer, String
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import Column, Integer, String, Text
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv('DB_URL')
@@ -23,3 +24,24 @@ async def save_user_data(tg_id: int, name: str, phone: str):
         user = User(tg_id=tg_id, name=name, phone=phone)
         session.add(user)
         await session.commit()
+
+
+
+
+class Vacancy(Base):
+    __tablename__ = "vacancies"
+
+    id = Column(Integer, primary_key=True, unique=True)
+    company = Column(String, nullable=True)
+    region = Column(String, nullable=True)
+    title = Column(String, nullable=False)
+    salary_from = Column(String, nullable=True)
+    salary_to = Column(String, nullable=True)
+    requirements = Column(Text, nullable=True)
+    responsibilities = Column(Text, nullable=True)
+    schedule = Column(String, nullable=True)
+    experience = Column(String, nullable=True)
+    employment = Column(String, nullable=True)
+    metro = Column(String, nullable=True)  # Сохраняем как строку, можно сериализовать список
+    url = Column(String, nullable=False)
+    employer_url = Column(String, nullable=True)
