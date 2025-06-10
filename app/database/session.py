54c1592736dfd0586586a11bc2b8049999f470eb
Ilya-Cheb0503 <cheb.ilya05@yandex.ru>
2025-06-10 21:24:26 +0300

    feat: add async_session using for db proccess

---
+++ app/database/session.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DB_URL")  # Убедись, что в .env есть DB_URL, например:
+# DB_URL=sqlite+aiosqlite:///./db.sqlite3
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+
+async_session = sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
