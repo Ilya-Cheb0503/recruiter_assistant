ebf823024d830e51bc6ff592ea62b633f83adc15
Paulina <fxcklleewyb@gmail.com>
2025-06-12 22:05:07 +0300

    feat: to add new routers

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from app.config import load_config
-from app.handlers import admin, common, job_seeking, start
+from app.handlers import admin, job_seeking, start, main_menu, about_company
 from app.handlers.form import form_fill, agreement  # Импорт твоего роутера
 
 async def main():
@@ -13,9 +13,10 @@ async def main():
 
     dp.include_router(start.router)
     dp.include_router(admin.router)
-    dp.include_router(common.router)
+    dp.include_router(main_menu.router)
     dp.include_router(job_seeking.router)
     dp.include_router(form_fill.router)
     dp.include_router(agreement.router)
+    dp.include_router(about_company.router)
 
     await dp.start_polling(bot)
