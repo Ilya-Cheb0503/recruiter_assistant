2795d59a776107be9fd30c10b1043dfed52182fc
Paulina <fxcklleewyb@gmail.com>
2025-06-12 20:32:46 +0300

    chore: to add the right text

--- app/bot_text/contacts.py
+++ app/bot_text/contacts.py
@@ -1,1 +1,7 @@
-contact_info_text = "контакты тут"
+contact_info_text = '''
+Если у вас остались вопросы — просто пишите нам, мы вам будем рады!
+
+Контакты 📬
+✉️ rabotaNZL@reph.ru
+🌐 www.nzl.ru
+'''
