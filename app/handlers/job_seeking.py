a66b627132438feaba696bc4e645fa7ecf1ad09e
Paulina <fxcklleewyb@gmail.com>
2025-06-14 11:50:57 +0300

    chore: to try fix the back button

--- app/handlers/job_seeking.py
+++ app/handlers/job_seeking.py
@@ -8,6 +8,7 @@ from services.vacancy_service import get_all_vacancies
 from utils.vacancy_sender import send_vacancy_batch
 from services.vacancy_service import get_vacancies_by_keywords_list, get_vacancies_no_experience
 from app.keywords.categories_keywords import keywords
+from aiogram.fsm.context import FSMContext
 
 
 router = Router()
@@ -25,13 +26,25 @@ async def region_selected_handler(callback: CallbackQuery, state: FSMContext):
         reply_markup=get_job_seeking_menu()
     )
 
+
+# @router.callback_query(F.data == 'job_search_with_current_region')
+# async def job_search_handler(callback: CallbackQuery, state: FSMContext):
+#     data = state.get_data()
+#     region = data["region"]
+#     print(region)
+
+#     await callback.answer()
+#     await callback.message.edit_text(
+#         f"Выбран регион: {region}\nТеперь выберите, как искать вакансии:",
+#         reply_markup=get_job_seeking_menu()
+#     )
+
     
 @router.callback_query(F.data == 'categories_vacancies')
 async def vacancies_by_keywords_handler(callback: CallbackQuery):
     await callback.answer()
     await callback.message.edit_text("Выберите подраздел", reply_markup=get_job_by_categories_menu())
 
-from aiogram.fsm.context import FSMContext
 
 @router.callback_query(F.data.startswith("vacancies_"))
 async def vacancy_pagination_handler(callback: CallbackQuery, state: FSMContext):
