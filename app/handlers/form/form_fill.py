d8b24eec1ebf5d76c7c00fa9337536c512cee9a3
Paulina <fxcklleewyb@gmail.com>
2025-06-21 20:16:19 +0300

    feat: to add checking the user's region for incoming message

--- app/handlers/form/form_fill.py
+++ app/handlers/form/form_fill.py
@@ -7,10 +7,12 @@ from aiogram.types import CallbackQuery, Message
 from app.database.models import save_user_data
 from app.keyboards.inline.form_keyboard import confim_button
 from app.keyboards.inline.menu import get_main_menu
-from app.keyboards.reply.form_keyboard import (education_level_button,
-                                               experience_button,
-                                               get_edit_fields_keyboard,
-                                               region_button)
+from app.keyboards.reply.form_keyboard import (
+    education_level_button,
+    experience_button,
+    get_edit_fields_keyboard,
+    region_button,
+)
 from app.services.static_content import load_content
 from app.states.state_user_form import FormStates
 from app.utils.format_form import format_user_form
@@ -23,48 +25,54 @@ router = Router()
 @router.message(FormStates.waiting_name)
 async def get_name(msg: Message, state: FSMContext):
     await state.update_data(name=msg.text)
-    await msg.answer('Ваш контактный номер телефона:')
+    await msg.answer("Ваш контактный номер телефона:")
     await state.set_state(FormStates.waiting_phone)
 
 
 @router.message(FormStates.waiting_phone)
 async def get_phone_number(msg: Message, state: FSMContext):
     if not is_valid_phone(msg.text):
-        await msg.answer("❗️Неверный формат номера телефона.\nПожалуйста, укажите Ваш контактный номер телефона в формате +7-***-***-**-**")
+        await msg.answer(
+            "❗️Неверный формат номера телефона.\nПожалуйста, укажите Ваш контактный номер телефона в формате +7-***-***-**-**"
+        )
         return
     await state.update_data(phone=msg.text)
-    await msg.answer("Пожалуйста, укажите Ваш электронный адрес в формате ivanov.ivan@mail.ru")
+    await msg.answer(
+        "Пожалуйста, укажите Ваш электронный адрес в формате ivanov.ivan@mail.ru"
+    )
     await state.set_state(FormStates.waiting_email)
 
 
 @router.message(FormStates.waiting_email)
 async def get_email(msg: Message, state: FSMContext):
     if not is_valid_email(msg.text):
-        await msg.answer("❗️Неверный формат электронного адреса.\nПожалуйста, укажите email в формате ivanov.ivan@mail.ru")
+        await msg.answer(
+            "❗️Неверный формат электронного адреса.\nПожалуйста, укажите email в формате ivanov.ivan@mail.ru"
+        )
         return
     await state.update_data(email=msg.text)
-    await msg.answer('Выбранный регион поиска:', reply_markup=region_button())
+    await msg.answer("Выбранный регион поиска:", reply_markup=region_button())
     await state.set_state(FormStates.waiting_region)
 
 
 @router.message(FormStates.waiting_region)
 async def get_user_region(msg: Message, state: FSMContext):
     await state.update_data(region=msg.text)
-    await msg.answer('Желаемая должность:')
+    await msg.answer("Желаемая должность:")
     await state.set_state(FormStates.waiting_position)
 
 
 @router.message(FormStates.waiting_position)
 async def get_position(msg: Message, state: FSMContext):
     await state.update_data(position=msg.text)
-    await msg.answer('Ваш стаж:', reply_markup=experience_button())
+    await msg.answer("Ваш стаж:", reply_markup=experience_button())
     await state.set_state(FormStates.waiting_experience)
 
 
 @router.message(FormStates.waiting_experience)
 async def get_experience(msg: Message, state: FSMContext):
     await state.update_data(experience=msg.text)
-    await msg.answer('Ваш уровень образования:', reply_markup=education_level_button())
+    await msg.answer("Ваш уровень образования:", reply_markup=education_level_button())
     await state.set_state(FormStates.waiting_education_level)
 
 
@@ -82,16 +90,26 @@ async def confirm_form(callback: CallbackQuery, state: FSMContext):
 
     # Фильтрация только нужных полей
     allowed_fields = {
-        "name", "phone", "email", "region",
-        "position", "experience", "education"
+        "name",
+        "phone",
+        "email",
+        "region",
+        "position",
+        "experience",
+        "education",
     }
     filtered_data = {k: v for k, v in data.items() if k in allowed_fields}
 
     await save_user_data(callback.from_user.id, **filtered_data)
-    await callback.message.edit_text("Спасибо! Анкета заполнена ✅\nВозвращаем вас в Главное меню.")
+    await callback.message.edit_text(
+        "Спасибо! Анкета заполнена ✅\nВозвращаем вас в Главное меню."
+    )
     await asyncio.sleep(2)
     content = load_content()
-    await callback.message.edit_text(content.get('welcome_message')[0], reply_markup=get_main_menu(callback.from_user.id))
+    await callback.message.edit_text(
+        content.get("welcome_message")[0],
+        reply_markup=get_main_menu(callback.from_user.id),
+    )
     await state.clear()
     await callback.answer()
     await log_event(user_id=callback.from_user.id, event_type="form_submit")
@@ -99,8 +117,10 @@ async def confirm_form(callback: CallbackQuery, state: FSMContext):
 
 @router.callback_query(F.data == "form_edit", FormStates.waiting_confirm)
 async def edit_form(callback: CallbackQuery, state: FSMContext):
-    await callback.message.edit_text("Что вы хотите изменить?", reply_markup=get_edit_fields_keyboard())
-    await state.set_state(FormStates.waiting_field_to_edit)
+    await callback.message.edit_text(
+        "Что вы хотите изменить?", reply_markup=get_edit_fields_keyboard()
+    )
+    await state.set_state(FormStates.waitinЯФсвУК6Н)
     await callback.answer()
 
 
@@ -112,10 +132,25 @@ async def handle_field_choice(callback: CallbackQuery, state: FSMContext):
         "name": ("name", "Введите ваше ФИО:", None, "inline"),
         "phone": ("phone", "Введите номер телефона:", None, "inline"),
         "email": ("email", "Введите email:", None, "inline"),
-        "region": ("region", "Введите предпочтительный регион:", region_button(), "reply"),
+        "region": (
+            "region",
+            "Введите предпочтительный регион:",
+            region_button(),
+            "reply",
+        ),
         "position": ("position", "Введите вашу должность:", None, "inline"),
-        "experience": ("experience", "Введите опыт работы:", experience_button(), "reply"),
-        "education": ("education", "Введите уровень образования:", education_level_button(), "reply")
+        "experience": (
+            "experience",
+            "Введите опыт работы:",
+            experience_button(),
+            "reply",
+        ),
+        "education": (
+            "education",
+            "Введите уровень образования:",
+            education_level_button(),
+            "reply",
+        ),
     }
 
     if field_key not in field_map:
@@ -137,7 +172,6 @@ async def handle_field_choice(callback: CallbackQuery, state: FSMContext):
     await callback.answer()
 
 
-
 @router.message(FormStates.waiting_field_value)
 async def receive_new_value(msg: Message, state: FSMContext):
     data = await state.get_data()
@@ -148,10 +182,14 @@ async def receive_new_value(msg: Message, state: FSMContext):
 
     # Валидация email и телефона при редактировании
     if field == "phone" and not is_valid_phone(msg.text):
-        return await msg.answer("❗️Неверный формат номера телефона.\nПожалуйста, укажите номер в формате +7-***-***-**-**")
+        return await msg.answer(
+            "❗️Неверный формат номера телефона.\nПожалуйста, укажите номер в формате +7-***-***-**-**"
+        )
 
     if field == "email" and not is_valid_email(msg.text):
-        return await msg.answer("❗️Неверный формат email.\nПожалуйста, укажите адрес в формате ivanov.ivan@mail.ru")
+        return await msg.answer(
+            "❗️Неверный формат email.\nПожалуйста, укажите адрес в формате ivanov.ivan@mail.ru"
+        )
 
     await state.update_data({field: msg.text})
     updated_data = await state.get_data()
@@ -162,6 +200,8 @@ async def receive_new_value(msg: Message, state: FSMContext):
 @router.callback_query(F.data == "back_to_confirm", FormStates.waiting_field_to_edit)
 async def back_to_confirmation(callback: CallbackQuery, state: FSMContext):
     data = await state.get_data()
-    await callback.message.edit_text(format_user_form(data), reply_markup=confim_button())
+    await callback.message.edit_text(
+        format_user_form(data), reply_markup=confim_button()
+    )
     await state.set_state(FormStates.waiting_confirm)
     await callback.answer()
--- app/handlers/incoming_message.py
+++ app/handlers/incoming_message.py
@@ -6,28 +6,59 @@ from aiogram.fsm.context import FSMContext
 
 from app.states.state_user_form import FormStates
 from app.services.vacancy_service import get_vacancies_by_keyword_and_region
+from app.keyboards.reply.form_keyboard import region_button
+
 from utils.vacancy_sender import send_vacancy_batch
 
 
 router = Router()
 
 
+@router.message(FormStates.waiting_for_region)
+async def get_first_region(msg: Message, state: FSMContext):
+    print(f"Функция {get_first_region.__name__}")
+    user_data = await state.get_data()
+    keyword = user_data.get("user_keyword")
+
+    await state.update_data(region=msg.text)
+    await vacansies_transmission(msg, user_keyword=keyword, region=msg.text)
+
+
 @router.message(F.text)
 async def get_incoming_message(msg: Message, state: FSMContext):
+    print(f"Функция {get_incoming_message.__name__}")
     current_state = await state.get_state()
 
     if msg.text.lower() in ["/start", "главное меню"]:
         return
     elif current_state and current_state.startswith("FormStates"):
         return
+    await check_region(msg, state)
+    print(f"Функция {get_incoming_message.__name__} завершилась")
+    
+
+async def check_region(msg: Message, state: FSMContext):
+    print(f"Функция {check_region.__name__}")
 
     # Получаем сохранённый регион пользователя из FSM
     user_data = await state.get_data()
-    user_region = user_data.get("region", "Москва")  # дефолт если нет данных
+    user_region = user_data.get("region")
+    await state.update_data(user_keyword=msg.text)
 
-    vacancies = await get_vacancies_by_keyword_and_region(msg.text, user_region)
+    if user_region is None:
+        await msg.answer(text="Вы не указали регион для поиска вакансий❌\nВыберет регион из предложенных вариантов.", reply_markup=region_button())
+        await state.set_state(FormStates.waiting_for_region)
+        print(f"Функция {check_region.__name__} перед return")
+        return
+    
+    await vacansies_transmission(msg, user_keyword=msg.text, region=user_region)
+
+
+async def vacansies_transmission(msg: Message, user_keyword: str, region: str):
+    print(f"Функция {vacansies_transmission.__name__}")
+
+    vacancies = await get_vacancies_by_keyword_and_region(user_keyword, region)
     await send_vacancy_batch(
     message=msg,
     vacancies=vacancies
     )
-        
